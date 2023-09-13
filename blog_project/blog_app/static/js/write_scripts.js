0772a0cdf5a4559f3c384d84155874ac02c8c41c
nnonamii <chloenoh91@gmail.com>
2023-09-14 08:16:57 +0900

    Update Write

--- blog_project/blog_app/static/js/write_scripts.js
+++ blog_project/blog_app/static/js/write_scripts.js
@@ -1,19 +1,19 @@
-tinymce.init({
-    selector: 'textarea#default',
-    plugins: 'autoresize',
-    width: 900,
-    height: 600,
-    plugins: [
-        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'prewiew', 'anchor', 'pagebreak',
-        'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
-        'table', 'emoticons', 'template', 'codesample'
-    ],
-    toolbar: 'undo redo | styles | bold italic underline | alignleft aligncenter alignright alignjustify |' +
-        'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
-        'forecolor backcolor emoticons',
-    menu: {
-        favs: { title: 'menu', items: 'code visualaid | searchreplace | emoticons' }
-    },
-    menubar: 'favs file edit view insert format tools table',
-    content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px}'
-});
+// tinymce.init({
+//     selector: 'textarea#default-editor',
+//     plugins: 'autoresize',
+//     width: 700,
+//     height: 500,
+//     plugins: [
+//         'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'prewiew', 'anchor', 'pagebreak',
+//         'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
+//         'table', 'emoticons', 'template', 'codesample'
+//     ],
+//     toolbar: 'undo redo | styles | bold italic underline | alignleft aligncenter alignright alignjustify |' +
+//         'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+//         'forecolor backcolor emoticons',
+//     menu: {
+//         favs: { title: 'menu', items: 'code visualaid | searchreplace | emoticons' }
+//     },
+//     menubar: 'favs file edit view insert format tools table',
+//     content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px}'
+// });
