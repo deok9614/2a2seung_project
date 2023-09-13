d03960417ac9cc5acf49fd38d670cf9863358034
deok <dudejr9614@gmail.com>
2023-09-14 08:35:05 +0900

    commit
    deok

---
+++ blog_project/blog_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views  # 로그인 된 사용자만 권한 부여하기
+
+urlpatterns = [
+    path("admin", admin.site.urls),
+    path("board_client", views.board_client, name="board_client"),
+    path("board_admin", views.board_admin, name="board_admin"),
+    # path("", include("tinymce.urls")),
+]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def board_client(request):
+    return render(request, 'blog_app/board_client.html')
+
+def board_admin(request):
+    return render(request, 'blog_app/board_admin.html')
